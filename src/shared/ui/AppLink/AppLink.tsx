3d1f0d6096dff4bbe7852f6014e763717da826e4
Bohdan-Dubravin <dubravin111@gmail.com>
2023-09-23 20:32:43 +0300

    add navbar

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,34 @@
+import classNames from '@shared/lib/classNames/classNames';
+import { LinkProps } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import cls from './AppLink.module.scss';
+
+export enum AppLinkTheme {
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary',
+}
+
+interface AppLinkProps extends LinkProps {
+  className?: string;
+  theme?: AppLinkTheme;
+}
+
+const AppLink = ({
+  to,
+  className,
+  children,
+  theme = AppLinkTheme.PRIMARY,
+  ...otherProps
+}: AppLinkProps) => {
+  return (
+    <Link
+      to={to}
+      className={classNames(cls.AppLink, {}, [className, cls[theme]])}
+      {...otherProps}
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default AppLink;
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,7 @@
 import classNames from '@shared/lib/classNames/classNames';
-import { Link } from 'react-router-dom';
+
+import cls from './NavBar.module.scss';
+import AppLink, { AppLinkTheme } from '@shared/ui/AppLink/AppLink';
 
 interface NavbarProps {
   className?: string;
@@ -7,8 +9,13 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
   return (
-    <nav className={classNames('navbar', {}, [className])}>
-      <Link to="/about">about</Link>
+    <nav className={classNames(cls.navbar, {}, [className])}>
+      <div className={cls.links}>
+        <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
+          about
+        </AppLink>{' '}
+        <AppLink to="/about">about</AppLink>
+      </div>
     </nav>
   );
 };
